5c9e95103b4b0dc6b5344182549a61c08eaaea4d
karn21 <karn212000@gmail.com>
2021-04-14 14:53:28 +0530

    make output filename unique

--- Img_to_pdf/img_to_pdf.py
+++ Img_to_pdf/img_to_pdf.py
@@ -1,29 +1,45 @@
 import os
 from PIL import Image
 from fpdf import FPDF
+import datetime
 
+# Page Size
 A4_HEIGHT = 842
 A4_WIDTH = 595
 
 INPUT_PATH = "input/"
 
 def img_to_pdf():
+	# initialise fpdf object
 	pdf = FPDF(unit="pt",format=[A4_WIDTH,A4_HEIGHT])
+
 	for file_name in os.listdir(INPUT_PATH):
+
 		file_path = INPUT_PATH+file_name
 		print("Merging",file_name,"to pdf...")
 		img = Image.open(file_path)
 		width,height = img.size
+
+		# convert px to pt
 		width = width/1.36
 		height = height/1.36
+
+		# resize if bigger than page size
 		while(height>A4_HEIGHT or width>A4_WIDTH):
 			width = width/1.2
 			height = height/1.2
+
+		# calculate offset for centering image
 		x_offset = (A4_WIDTH - width)/2
 		y_offset =  (A4_HEIGHT - height)/2
+
 		pdf.add_page()
 		pdf.image(file_path,x=x_offset,y=y_offset,w=width,h=height)
 		img.close()
-	pdf.output("output/op.pdf","F")
+
+	# use timestamp for unique filename
+	timestamp = datetime.datetime.now()
+	filename = timestamp.strftime("%d-%b-%Y_(%H-%M-%S)")
+	pdf.output(f"output/{filename}.pdf","F")
 
 img_to_pdf()
