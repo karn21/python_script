2461c7c13e044d044653503524cf5f85fe46e800
karn21 <karn212000@gmail.com>
2021-04-14 15:28:11 +0530

    ad comments

--- Pdf_merger/pdf_merger.py
+++ Pdf_merger/pdf_merger.py
@@ -6,12 +6,16 @@ INPUT_PATH = "input/"
 
 def pdf_merger():
 	merger = PdfFileMerger()
+
+	# open and append all pdf files in input folder
 	for file_name in glob(f"{INPUT_PATH}*.pdf"):
 		print(f"Merging file {file_name} to pdf")
 		merger.append(file_name)
+	# create output file with name as timestamp
 	timestamp = datetime.datetime.now()
 	filename = timestamp.strftime("%d-%b-%Y_(%H-%M-%S)")
 	op = open(f"output/{filename}","wb")
+	
 	merger.write(op)
 	merger.close()
 
